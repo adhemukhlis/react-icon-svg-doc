31ff83b3b8f4721b0cb603b9ba6dc929599bacdd
adhemukhlis <adhemukhlis@gmail.com>
2018-12-24 23:02:11 +0700

    #init

---
+++ precache-manifest.d2731cf59f707b867db45dbeb09a5036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb9fa1a74d27eae13888",
+    "url": "/static/js/main.cb9fa1a7.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "cb9fa1a74d27eae13888",
+    "url": "/static/css/main.e603101f.chunk.css"
+  },
+  {
+    "revision": "d86260d0bf1330fbf9dcbe687e973155",
+    "url": "/index.html"
+  }
+];
