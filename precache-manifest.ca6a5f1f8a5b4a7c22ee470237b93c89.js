484ace57a369de2ca8e098f268d2e40bd204699d
adhemukhlis <adhemukhlis@gmail.com>
2020-03-16 17:30:39 +0700

    160320201

---
+++ precache-manifest.ca6a5f1f8a5b4a7c22ee470237b93c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee284c759a1682a49004844c6e9d082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee6fbb8ecca8ee927f9",
+    "url": "/static/css/main.487d757d.chunk.css"
+  },
+  {
+    "revision": "8ca64e199ff32033b1cd",
+    "url": "/static/js/2.23560e12.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.23560e12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bee6fbb8ecca8ee927f9",
+    "url": "/static/js/main.9764153f.chunk.js"
+  },
+  {
+    "revision": "0bd52deff8d2f09aa639",
+    "url": "/static/js/runtime-main.9ac06b47.js"
+  },
+  {
+    "revision": "58217973c7d1b9a8b4ae08df3ccefe70",
+    "url": "/static/media/carbon.58217973.svg"
+  }
+]);
